cf087ab66ed5bead64c36ab37016fe0492b11558
Lonnie Jones <lonnie34j@gmail.com>
2016-08-05 05:53:26 -0400

    initial save files

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -3,6 +3,7 @@ var characters = ["ryu","ken","sagat","chunli","mbison","balrog","ehonda","guile
 var userWins = 0;
 var triesRemaining = 6;
 
+
 alphabet = [];
 for (i = 97; i <= 122; i++){
     alphabet[alphabet.length] = String.fromCharCode(i);
@@ -12,6 +13,8 @@ console.log(alphabet);
 //takes users key presses
 document.onkeyup = function(event) { 
 	var lettersGuessed = String.fromCharCode(event.keyCode).toLowerCase();
+//replaces placeholder text on page with user keys
+	document.getElementById("currentwordP").innerHTML=lettersGuessed;
 
 console.log(lettersGuessed);
 
@@ -36,6 +39,3 @@ function randomWord() {
 }
 randomWord();
 
-function replaceHolder() {
-	if(lettersGuessed==placeholder)
-}
