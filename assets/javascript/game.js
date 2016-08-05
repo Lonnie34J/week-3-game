1b72a29026ca41ce4948a844a46f10b13ddd6a5e
Lonnie Jones <lonnie34j@gmail.com>
2016-08-05 02:23:58 -0400

    initial save files

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -3,11 +3,18 @@ var characters = ["ryu","ken","sagat","chunli","mbison","balrog","ehonda","guile
 var userWins = 0;
 var triesRemaining = 6;
 
+alphabet = [];
+for (i = 97; i <= 122; i++){
+    alphabet[alphabet.length] = String.fromCharCode(i);
+}
+console.log(alphabet);
+
 //takes users key presses
 document.onkeyup = function(event) { 
 	var lettersGuessed = String.fromCharCode(event.keyCode).toLowerCase();
-	
-alert(lettersGuessed);
+
+console.log(lettersGuessed);
+
 
 }
 
@@ -16,9 +23,19 @@ alert(lettersGuessed);
 function randomWord() {
 	var currentWord = characters[Math.floor(Math.random()*characters.length)];
 	console.log(currentWord);
-	
+	//	
+	var placeholder="";
+	for (var i=0; i<currentWord.length; i++){
+		placeholder=placeholder+"_";
+		console.log(placeholder);
+	}
+
 	//sets the random word on html page
 	newCurrent=document.getElementById("currentwordP");
-	newCurrent.innerHTML=currentWord;
+	newCurrent.innerHTML=placeholder;
 }
 randomWord();
+
+function replaceHolder() {
+	if(lettersGuessed==placeholder)
+}
