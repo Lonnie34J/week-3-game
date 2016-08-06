d3c4090d9be4383542b1d2c448d6e902b3090b6e
Lonnie Jones <lonnie34j@gmail.com>
2016-08-06 16:41:45 -0400

    comment

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -3,44 +3,37 @@ var characters = ["ryu","ken","sagat","chunli","mbison","balrog","ehonda","guile
 var userWins = 0;
 var triesRemaining = 6;
 var letters = "abcdefghijklmnopqrstuvwxyz";
-var lettersArray = letters.split("");
-console.log(lettersArray);
-var guessedArray = [];
-var userGuess=guessedArray;
+var currentWord;
+var placeholder;
 
 
 
 //takes users key presses
 document.onkeyup = function(event) { 
-	var lettersGuessed = String.fromCharCode(event.keyCode).toLowerCase();
-
-
-//places letters in already guessed section
-var lettersUsed=document.getElementById("guessed");
-lettersUsed.innerHTML=guessedArray + lettersGuessed;
-
-guessedArray.push(lettersGuessed);
-console.log(guessedArray);
-
-var userGuess=lettersGuessed;
+	var guessed = String.fromCharCode(event.keyCode).toLowerCase();
+	var guessedPosition= currentWord.indexOf(guessed);
+	if (guessedPosition==-1) {
+		triesRemaining--;
+	}else{
+		var correctGuess=[];
+		correctGuess.push(guessedPosition);
+	}
+	console.log(guessedPosition);
+	console.log(correctGuess);
 }
 
 
 //picks a random word from characters array
 function randomWord() {
-	var currentWord = characters[Math.floor(Math.random()*characters.length)];
+	currentWord = characters[Math.floor(Math.random()*characters.length)];
 	console.log(currentWord);
 	//	
-	var placeholder="";
+	placeholder="";
 	for (var i=0; i<currentWord.length; i++){
 		placeholder=placeholder+"_";
-		console.log(placeholder);
-		//logs the current word's characters at their indexes
-		console.log(currentWord.charAt(i));	
-
+		
 	}
-
-
+	console.log(currentWord.indexOf(guessed));
 
 	//sets the random word on html page
 	newCurrent=document.getElementById("currentwordP");
